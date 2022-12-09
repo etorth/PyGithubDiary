d8e2b2b4dff7be1b37a55f43dbfdcb6b4ede16f8
etorth <anhonghe@gmail.com>
2022-12-09 12:45:07 -0800

    init commit

---
+++ plugin/PyGithubDiary.vim
@@ -0,0 +1,81 @@
+if !has('python3')
+    echohl ErrorMsg | echomsg 'PyGithubDiary: python3 is required but not available.' | echohl None
+    finish
+endif
+
+function! s:Diary_createDiaryInst()
+python3 << PY3_EOF
+
+import os
+import PyGithubDiary
+
+if 'g_diaryInst' not in locals():
+    try:
+        homeDir = os.environ['HOME']
+    except KeyError:
+        homeDir = os.environ['HomePath']
+    except:
+        raise RuntimeError('No valid home path configured in os envs')
+
+    jsonPath = homeDir + '/.diary.json'
+    if not os.path.isfile(jsonPath):
+        raise RuntimeError('Config file does not exist: %s' % jsonPath)
+
+    g_diaryInst = PyGithubDiary.Diary(jsonPath)
+
+PY3_EOF
+endfunction
+
+function! s:DiaryFunc_create()
+    call s:Diary_createDiaryInst()
+
+    tabnew
+    norm gg
+
+    put! =py3eval('g_diaryInst.create_content()')
+
+    norm G
+    let t:PyGithubDiary_tab_opened = 1
+endfunction
+
+function! s:DiaryFunc_submit()
+    if exists('t:PyGithubDiary_tab_opened')
+        let l:py_cmd = printf('g_diaryInst.submit(%s%s%s)', '"""', join(getline(1, '$'), '\n'), '"""')
+        call py3eval(l:py_cmd)
+
+        unlet t:PyGithubDiary_tab_opened
+        q!
+    else
+        echohl ErrorMsg | echomsg 'PyGithubDiary: current tab is not opened to submit diary.' | echohl None
+    end
+endfunction
+
+function! s:DiaryFunc_viewText(regfile)
+    call s:Diary_createDiaryInst()
+
+    tabnew
+    norm gg
+
+    let l:py_cmd = printf('g_diaryInst.view_text(%s%s%s)', '"""', a:regfile, '"""')
+    put! =py3eval(l:py_cmd)
+
+    norm gg
+endfunction
+
+function! s:DiaryFunc_viewHtml(regfile)
+    call s:Diary_createDiaryInst()
+
+    tabnew
+    norm gg
+
+    let l:py_cmd = printf('g_diaryInst.view_html(%s%s%s)', '"""', a:regfile, '"""')
+    put! =py3eval(l:py_cmd)
+
+    set filetype=html
+    norm gg
+endfunction
+
+command!          DiaryCreate   :call s:DiaryFunc_create()
+command!          DiarySubmit   :call s:DiaryFunc_submit()
+command! -nargs=1 DiaryViewText :call s:DiaryFunc_viewText(<q-args>)
+command! -nargs=1 DiaryViewHtml :call s:DiaryFunc_viewHtml(<q-args>)
---
+++ python3/PyGithubDiary.py
@@ -0,0 +1,301 @@
+import os
+import re
+import zlib
+import json
+import html
+import base64
+import github
+import socket
+import datetime
+import threading
+import collections
+import concurrent.futures
+
+
+class Diary:
+
+
+    def __init__(self, json_path):
+        with open(json_path) as json_file:
+            self.config = json.load(json_file)
+
+            self.config.setdefault('log-path', None)
+            self.config.setdefault('diary-repository', 'diary')
+            self.config.setdefault('use-base64-encryption', True)
+            self.config.setdefault('allow-public-repository', False)
+
+            if self.config['log-path']:
+                self.logLock = threading.Lock()
+                self.logFile = open(self.config['log-path'], 'a')
+
+            self.log('PyGithubDiary starts with PID %d' % os.getpid())
+            self.log('')
+
+            try:
+                self.gh = github.Github(self.config['github-token'])
+            except github.BadCredentialsException:
+                raise ValueError('invalid github token')
+
+            try:
+                self.repo_handler = self.gh.get_user().get_repo(self.config['diary-repository'])
+            except github.UnknownObjectException:
+                self.repo_handler = self.gh.get_user().create_repo(self.config['diary-repository'], description='Created by etorth/PyGithubDiary', private=True, auto_init=True)
+
+            if (not self.config['allow-public-repository']) and (not self.repo_handler.private):
+                raise ValueError('%s/%s is a public repository, set \'allow-public-repository\' as true to bypass' % (self.gh.get_user().login, self.config['diary-repository']))
+
+
+    def log(self, msg: str):
+        if self.config['log-path']:
+            timestamp = self.now()
+            lines = msg.splitlines()
+            
+            # don't do strip for lines
+            # if there are unexpected blankspaces, it means content itself needs update
+
+            self.logLock.acquire()
+            for line in lines:
+                if line:
+                    self.logFile.write('%s: %s\n' % (timestamp, line))
+                else:
+                    self.logFile.write('%s:\n' % timestamp)
+            self.logLock.release()
+
+
+    def today(self) -> str:
+        return datetime.date.today().strftime("%Y.%m.%d")
+
+
+    def today_file_name(self):
+        return self.today() + ".txt"
+
+
+    def now(self):
+        return str(datetime.datetime.now())
+
+
+    def weekday(self):
+        return datetime.date.today().strftime("%A")
+
+
+    def title(self):
+        return self.now() + " " + self.weekday() + " wrote:"
+
+
+    def create_content(self) -> str:
+        try:
+            today_file_content = self.repo_handler.get_contents(self.today_file_name())
+        except github.UnknownObjectException:
+            return self.title()
+        else:
+            return self.decode(self.get_file_handler_content(today_file_content)).strip() + "\n\n\n" + self.title()
+
+
+    def pull_file_content(self, file_handler, name_pattern):
+        if file_handler.type == 'dir':
+            raise ValueError('unexpected directory %s' % file_handler.path)
+        elif file_handler.path.endswith('.txt') and name_pattern.fullmatch(file_handler.path[:-4]):
+            # pull diary file content and decode
+            # done call .strip() before .decode(), tailing '\n' works as non-compression marker
+            return (file_handler.path, self.decode(self.get_file_handler_content(file_handler)))
+
+
+    def get_file_handler_content(self, file_handler) -> str:
+        if file_handler.encoding == 'base64':
+            return file_handler.decoded_content.decode("utf-8")
+        else:
+            # github doesn't support to directly retrieve large files > 1 MBytes
+            # use blob type to retrieve file content
+            #
+            #   https://github.com/PyGithub/PyGithub/issues/661
+            #
+            # but still, currently gitbub doesn't support file > 100 MBytes
+            #
+            #   https://docs.github.com/en/rest/repos/contents
+            #
+            # see section of 'size limit'
+            #
+            # if oneday this method still not working, try following way
+            #
+            #   curl -H 'Accept: application/vnd.github.raw' \
+            #        -H 'Authorization: token <your-github-token>' \
+            #        'https://raw.githubusercontent.com/etorth/diary/main/2022.12.09.txt'
+            #
+            # where the url is from: ContentFile.download_url
+            # in python use package 'requests' instead of using the curl binary
+            return base64.b64decode(self.repo_handler.get_git_blob(file_handler.sha).content).decode('utf-8')
+
+
+    def pull_diaries(self, regname) -> list:
+        try:
+            file_handlers = self.repo_handler.get_contents('')
+        except github.UnknownObjectException:
+            return []
+        else:
+            if regname == 'today':
+                regname = self.today()
+
+            feats = []
+            file_pattern = re.compile(regname)
+
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                while len(file_handlers) > 0:
+                    feats.append(executor.submit(self.pull_file_content, file_handlers.pop(0), file_pattern))
+
+            # keep it if file exists but is empty
+            # this ensures to keep an entry when file name matches
+            return [f.result() for f in feats if f.result() is not None]
+
+
+    def view_text(self, regname) -> str:
+        text_lines = []
+        for name, content in self.pull_diaries(regname):
+            text_lines.append('<---------------------------%s----------------------------\n\n\n' % name[0:-4])
+            text_lines.append(content.rstrip())
+            text_lines.append('\n\n\n');
+
+        return ''.join(text_lines[0:-1])
+
+
+    def view_html(self, regname) -> str:
+        html_lines = []
+        html_lines.append('<!DOCTYPE html>')
+        html_lines.append('<html>')
+        html_lines.append('    <head>')
+        html_lines.append('        <meta charset="utf-8"/>')
+        html_lines.append('        <style>')
+        html_lines.append('            .diaryDiv {')
+        html_lines.append('                border: 2px outset red;')
+        html_lines.append('                background-color: lightblue;')
+        html_lines.append('            }')
+        html_lines.append('            .diaryDiv pre {')
+        html_lines.append('                white-space: pre-wrap;')
+        html_lines.append('                word-wrap: break-word;')
+        html_lines.append('            }')
+        html_lines.append('            .diaryDiv img {')
+        html_lines.append('                max-width: 100%;')
+        html_lines.append('                height: auto;')
+        html_lines.append('            }')
+        html_lines.append('        </style>')
+        html_lines.append('    </head>')
+        html_lines.append('    <body>')
+
+        pattern_imgbase64 = re.compile('^\s*\[\[imgbase64\s+?(\S+)\s*\]\]\s*$')
+        pattern_timestamp = re.compile('^\s*(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{6} .* wrote:)\s*$')
+
+        for name, content in self.pull_diaries(regname):
+            last_empty = False
+            last_timestamp = None
+
+            has_content = False
+            div_started = False
+
+            for line in content.splitlines():
+                striped_line = line.rstrip()
+                matched_timestamp = pattern_timestamp.match(striped_line)
+
+                if matched_timestamp:
+                    last_timestamp = matched_timestamp.group(1)
+
+                elif striped_line:
+                    has_content = True
+                    if not div_started:
+                        html_lines.append('        <div class="diaryDiv">')
+                        html_lines.append('            <h2 style="text-align:center; color:brown">%s</h2>' % name[:-4])
+                        div_started = True
+
+                    if last_timestamp:
+                        html_lines.append('            <h3 style="color:blue;">%s</h3>' % last_timestamp)
+                    elif last_empty:
+                        html_lines.append('            <br/>')
+
+                    # if there are both timestamp and empty line
+                    # only prints timestamp
+
+                    last_empty = False
+                    last_timestamp = None
+
+                    matched_imgbase64 = pattern_imgbase64.match(striped_line)
+                    if matched_imgbase64:
+                        html_lines.append('            <img alt="image" src="%s"/>' % matched_imgbase64.group(1))
+                    else:
+                        html_lines.append('            <pre>%s</pre>' % html.escape(striped_line))
+                else:
+                    last_empty = True
+
+            if has_content:
+                html_lines.append('        </div>')
+
+        html_lines.append('    </body>')
+        html_lines.append('</html>')
+        return '\n'.join(html_lines)
+
+
+    def submit(self, content):
+        self.log(content)
+        blob = self.repo_handler.create_git_blob(self.encode(self.translate_content(content.strip())), "utf-8")
+        element = github.InputGitTreeElement(path=self.today_file_name(), mode='100644', type='blob', sha=blob.sha)
+
+        branch_sha = self.repo_handler.get_branch(self.repo_handler.default_branch).commit.sha
+        base_tree = self.repo_handler.get_git_tree(sha=branch_sha)
+
+        tree = self.repo_handler.create_git_tree([element], base_tree)
+        parent = self.repo_handler.get_git_commit(sha=branch_sha)
+
+        commit = self.repo_handler.create_git_commit('update from ' + socket.gethostname(), tree, [parent])
+        self.repo_handler.get_git_ref('heads/' + self.repo_handler.default_branch).edit(commit.sha)
+
+
+    def translate_content(self, content: str) -> str:
+        pattern_imgpath = re.compile('^\s*\[\[img\s+?(\S+)\s*\]\]\s*$')
+        translated_lines = []
+        for line in content.splitlines():
+            matched_imgpath = pattern_imgpath.match(line)
+            if matched_imgpath:
+                try:
+                    with open(matched_imgpath.group(1), 'rb') as f:
+                        # didn't put image type here
+                        # should be data:image/png;base64,<base64-text-stream>
+
+                        # didn't replace tailing '=' by '%3D', chrome looks still working
+                        # but this web does replacement: https://greywyvern.com/code/php/binary2base64
+                        translated_lines.append('[[imgbase64 data:image;base64,%s]]' % base64.b64encode(f.read()).decode('utf-8'))
+                except:
+                    translated_lines.append(line)
+            else:
+                translated_lines.append(line)
+
+        return '\n'.join(translated_lines)
+
+
+    def base64_zip_encode(self, s: str) -> str:
+        return base64.b64encode(zlib.compress(s.encode('utf-8'))).decode('utf-8')
+
+
+    def base64_zip_decode(self, s: str) -> str:
+        return zlib.decompress(base64.b64decode(s.encode('utf-8'))).decode('utf-8')
+
+
+    def freq_remap(self, s: str) -> str:
+        freqs = collections.Counter(s).most_common()
+        order = [f[0] for f in freqs]
+
+        # Counter sort chars of same frequency by order of occurance
+        # this automatically maitains stability for back and forth remapping
+
+        remap = dict(zip(order, order[::-1]))
+        return ''.join([remap[ch] for ch in s])
+
+
+    def encode(self, s):
+        if self.config['use-base64-encryption']:
+            return self.freq_remap(self.base64_zip_encode(s)) + 'E'
+        else:
+            return s + '\n'
+
+
+    def decode(self, s):
+        if s[-1] == 'E':
+            return self.base64_zip_decode(self.freq_remap(s[0:-1]))
+        else:
+            return s[0:-1]
